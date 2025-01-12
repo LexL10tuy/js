9ebe474c6768a5c14238ef1652cb64edc68b7ff4
New_Lord_8951 <alexandrmanorali@gmail.com>
2025-01-12 19:24:57 +0300

    eeee

---
+++ DZ1/task1/task2/script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click', function(){
+    // const name = prompt("Введите свое имя: ")
+    const year = document.getElementById('nameInput').value;
+
+    if (year) {
+        message.textContent = `Вам ${2025 - year} года!`;
+    } else {
+        message.textContent = 'Пожалуйста, введите год рождения!';
+    }
+    // if (name) {
+    //     alert(`Привет, ${name}!`);
+    // } else {
+    //     ('Пожалуйста, введите свое имя!');
+    // }
+
+});
